0a475aa14b687f0fe3b05fb4324a764ea54d699f
Sam Terfa <samterfa@gmail.com>
2021-11-28 15:29:02 -0600

    initial function generation

---
+++ R/zzz.R
@@ -0,0 +1,360 @@
+
+parse_response <- function(response){
+  
+  response_content <- httr::content(response)
+  
+  if('object' %in% names(response_content) && response_content$object == 'list' && 'data' %in% names(response_content)){
+    return(response_content$data %>% purrr::map_dfr(~.x))
+  }
+  
+  response_content %>% purrr::flatten_df()
+}
+
+list_engines <- function(return_response = F){
+  
+  base_url <- 'https://api.openai.com/v1'
+  endpoint <- 'engines'
+  
+  response <- httr::GET(url = glue::glue('{base_url}/{endpoint}'),
+                        httr::add_headers(authorization = glue::glue('Bearer {Sys.getenv("openai_secret_key")}')))
+  
+  if(return_response) return(response)
+  
+  parse_response(response)
+}
+
+get_engine <- function(engine_id, return_response = F){
+  
+  base_url <- 'https://api.openai.com/v1'
+  endpoint <- glue::glue('engines/{engine_id}')
+  
+  response <- httr::GET(url = glue::glue('{base_url}/{endpoint}'),
+                        httr::add_headers(authorization = glue::glue('Bearer {Sys.getenv("openai_secret_key")}')))
+  
+  if(return_response) return(response)
+  
+  parse_response(response)
+}
+
+
+parse_endpoints <- function(){
+  
+  endpoints <-
+    rvest::read_html('API Reference - OpenAI API.html') %>% 
+    rvest::html_elements('.endpoint')
+  
+  endpoints_df <- tibble::tibble()
+  
+  for(endpoint in endpoints){
+    
+    endpoint_df <- tibble::tibble()
+    
+    endpoint_name <- 
+      endpoint %>%
+      rvest::html_elements('a h2') %>%
+      rvest::html_text() %>%
+      stringr::str_replace_all('Beta$', ' - Beta') %>%
+      stringr::str_to_title()
+    
+    endpoint_method <-
+      endpoint %>%
+      rvest::html_elements('.endpoint-method') %>%
+      rvest::html_text() %>%
+      toupper()
+    
+    endpoint_url <- 
+      endpoint %>%
+      rvest::html_elements('.endpoint-path') %>%
+      rvest::html_text()
+    
+    endpoint_description <-
+      endpoint %>%
+      rvest::html_elements('p') %>%
+      rvest::html_text() %>%
+      pluck(2) %>%
+      glue::glue_collapse('\n')
+    
+    endpoint_df <-
+      endpoint_df %>%
+      dplyr::bind_rows(
+        tibble::tibble(endpoint_name = endpoint_name,
+                       endpoint_method = endpoint_method,
+                       endpoint_url = endpoint_url,
+                       endpoint_description = endpoint_description)
+      )
+    
+    endpoint_params_sections <-
+      endpoint %>%
+      rvest::html_elements('.param-section')
+    
+    params_section_df <- tibble::tibble()
+    
+    for(params_section in endpoint_params_sections){
+      
+      params_section_title <-
+        params_section %>%
+        rvest::html_elements('h3') %>%
+        rvest::html_text()
+      
+      params_section_params <-
+        params_section %>%
+        rvest::html_elements('.api-ref-anchor-link-hover')
+      
+      params_df <- tibble::tibble()
+      
+      for(param in params_section_params){
+        
+        param_name <-
+          param %>% 
+          rvest::html_element('.param-name') %>%
+          rvest::html_text()
+        
+        param_data_type <-
+          param %>% 
+          rvest::html_element('.param-type') %>%
+          rvest::html_text()
+        
+        param_required <-
+          param %>%
+          rvest::html_element('.param-reqd') %>%
+          rvest::html_text()
+        
+        if(is.na(param_required)){
+          param_required <- F
+        }else{
+          param_required <- T
+        }
+        
+        param_description <-
+          param %>%
+          rvest::html_elements('p') %>%
+          rvest::html_text() %>%
+          glue::glue_collapse(" ")
+        
+        if(params_section_title %>% tolower() %>% stringr::str_detect('path')){
+          param_type = 'path'
+        }
+        
+        if(params_section_title %>% tolower() %>% stringr::str_detect('body')){
+          param_type = 'body'
+        }
+        
+        if(params_section_title %>% tolower() %>% stringr::str_detect('query')){
+          param_type = 'query'
+        }
+        
+        params_df <-
+          dplyr::bind_rows(
+            params_df,
+            tibble::tibble(
+              param_name = param_name,
+              param_type = param_type,
+              param_data_type = param_data_type,
+              param_required = param_required,
+              param_description = param_description
+            )
+          )
+      }
+      
+      params_section_df <-
+        dplyr::bind_rows(
+          params_section_df,
+          params_df
+        )
+    }
+    
+    
+    if(nrow(params_section_df) > 0){
+      endpoint_df <-
+        endpoint_df %>%
+        dplyr::bind_cols(
+          params_section_df
+        )
+    }
+    
+    endpoints_df <-
+      endpoints_df %>%
+      dplyr::bind_rows(
+        endpoint_df
+      )
+  }
+  
+  endpoints_df <-
+    endpoints_df %>%
+    mutate(across(param_required, ~ ifelse(is.na(param_required), F, param_required)))
+}
+
+generate_functions <- function(endpoints_df = parse_endpoints(), output_path = 'R/functions.R'){
+  
+  base_url <- 'https://api.openai.com/v1'
+  
+  functions_df <-
+    endpoints_df %>%
+    mutate(function_name = 
+             endpoint_name %>% 
+             snakecase::to_snake_case() %>% 
+             stringr::str_remove_all('_beta')) %>%
+    relocate(function_name, .before = 1) 
+  
+  functions_df_sub <-
+    functions_df %>% 
+    select(starts_with(c('function', 'endpoint'))) %>%
+    distinct()
+  
+  function_text <- "\n"
+  
+  # For each unique endpoint
+  for(i in 1:nrow(functions_df_sub)){
+    
+    function_name <- functions_df_sub$function_name[[i]]
+    endpoint_name <- functions_df_sub$endpoint_name[[i]]
+    endpoint_method <- functions_df_sub$endpoint_method[[i]]
+    endpoint_url <- functions_df_sub$endpoint_url[[i]]
+    endpoint_description <- functions_df_sub$endpoint_description[[i]]
+    
+    # Add Function Title
+    function_text <-
+      paste0(function_text, 
+             glue::glue("\n#' {endpoint_name}", .trim = F))
+    
+    # Add Function Description
+    function_text <-
+      paste0(function_text, 
+             glue::glue("\n#' {endpoint_description}", .trim = F))
+    
+    function_text <-
+      paste0(function_text, 
+             glue::glue("\n#'\n", .trim = F))
+    
+    functions_df_params <- 
+      functions_df %>% 
+      filter(function_name == !!function_name, !is.na(param_name)) %>% 
+      arrange(function_name, -param_required, param_name)
+    
+    # Add param info to documentation
+    if(nrow(functions_df_params) > 0){
+      
+      for(j in 1:nrow(functions_df_params %>% filter(!is.na(param_name)))){
+        
+        param_name <- functions_df_params$param_name[[j]]
+        param_type <- functions_df_params$param_type[[j]]
+        param_data_type <- functions_df_params$param_data_type[[j]]
+        param_required <- functions_df_params$param_required[[j]]
+        param_description <- functions_df_params$param_description[[j]] %>% stringr::str_replace_all('\n', ' ')
+        
+        # Add parameters to documentation text
+        function_text <-
+          paste0(function_text,
+                 glue::glue("#' @param {param_name} ({param_data_type}) {param_description} {ifelse(param_required, 'Required', '')}\n", .trim = F))
+        
+      }
+    }
+    
+    # Wrap up documentation text
+    function_text <-
+      paste0(function_text,
+             "#' @export\n")
+    
+    # Define function
+    function_text <-
+      paste0(function_text, 
+             glue::glue("{function_name} <- function(", .trim = F))
+    
+    # Add param info to function definition
+    if(nrow(functions_df_params) > 0){
+      
+      for(j in 1:nrow(functions_df_params %>% filter(!is.na(param_name)))){
+        
+        param_name <- functions_df_params$param_name[[j]]
+        param_type <- functions_df_params$param_type[[j]]
+        param_data_type <- functions_df_params$param_data_type[[j]]
+        param_required <- functions_df_params$param_required[[j]]
+        param_description <- functions_df_params$param_description[[j]]
+        
+        # Add parameters to function definition
+        function_text <-
+          paste0(function_text, 
+                 ifelse(param_required,
+                        glue::glue("{param_name}, "),
+                        glue::glue("{param_name} = NULL, ")
+                 )
+          )
+      }
+    }
+    
+    function_text <-
+      paste0(function_text, 
+             'return_response = F){\n\n'
+      )
+    
+    # Fill out function definition
+    function_text <- 
+      paste0(function_text, 
+             glue::glue("\tendpoint_url <- glue::glue('{endpoint_url}')\n\n", .trim = F))
+    
+    # Grab params by type
+    query_params <- 
+      functions_df_params %>%
+      filter(param_type == 'query') %>% 
+      select(starts_with('param'))
+    
+    body_params <- 
+      functions_df_params %>%
+      filter(param_type == 'body') %>% 
+      select(starts_with('param'))
+    
+    path_params <- 
+      functions_df_params %>%
+      filter(param_type == 'path') %>% 
+      select(starts_with('param'))
+    
+    # Update function_text with body param info
+    if(nrow(body_params) > 0){
+      function_text <-
+        paste0(function_text,
+               glue::glue("\tbody_params <- c({paste0('\"', paste(body_params$param_name, collapse = '\",\"'), '\"')})\n", .trim = F),
+               glue::glue("\tbody <- body_params %>% map(sym) %>% setNames(body_params)\n\n", .trim = F))
+    }else{
+      function_text <-
+        paste0(function_text,
+               glue::glue("\tbody <- NULL\n\n", .trim = F))
+    }
+    
+    # Update function_text with query param info
+    if(nrow(query_params) > 0){
+      function_text <-
+        paste0(function_text,
+               glue::glue("\tquery_params <- c({paste0('\"', paste(query_params$param_name, collapse = '\",\"'), '\"')})\n", .trim = F),
+               glue::glue("\tquery <- query_params %>% map(sym) %>% setNames(query_params)\n\n", .trim = F))
+    }else{
+      function_text <-
+        paste0(function_text,
+               glue::glue("\tquery <- NULL\n\n", .trim = F))
+    }
+    
+    function_text <-
+      paste0(function_text,
+             glue::glue("\tresponse <- httr::{endpoint_method}(url = endpoint_url, body = body, query = query, httr::add_headers(authorization = glue::glue('Bearer {{Sys.getenv(\"openai_secret_key\")}}')))"))
+    
+    function_text <-
+      paste0(function_text,
+             "\n\n\tif(return_response) return(response)")
+    
+    function_text <-
+      paste0(function_text,
+             "\n\n\tparse_response(response)"
+      )
+    
+    function_text <-
+      paste0(function_text,
+             "\n\n}\n\n\n")
+  }
+  
+  readr::write_lines(function_text, output_path, append = F)
+  
+  file.edit(output_path)
+  
+  invisible()
+}
+
+
