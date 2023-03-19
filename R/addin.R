b1a4f9e7c4c3f067114c58e31d73c02d83e720c1
Sam Terfa <samterfa@gmail.com>
2023-03-18 22:40:33 -0500

    Update addin.R

--- R/addin.R
+++ R/addin.R
@@ -1,6 +1,8 @@
 
 autocomplete_r_code <- function(prompt = rstudioapi::getConsoleEditorContext()$contents, debug = FALSE, reset = FALSE){
   
+  if(debug) message('Started...')
+  
   suppressPackageStartupMessages(
     library(tidyverse)
   )
@@ -42,38 +44,60 @@ autocomplete_r_code <- function(prompt = rstudioapi::getConsoleEditorContext()$c
   total_request_chars <-
     openai_completions %>% purrr::keep(~ .x$role == 'user') %>% purrr::map_chr(~ .x$content) %>% paste(collapse = '') %>% nchar()
   
-  completion <- 
+  
+  if(debug) message('Making request...')
+  
+  resp <- 
     openai::create_chat_completion(
       messages = openai_completions, 
       model = 'gpt-3.5-turbo', 
       n = 1,
       max_tokens = 4096 - openai_completions_usage - 1, 
-      stream = F)
+      stream = F, 
+      return_response = TRUE)
   
-  openai_completions <<-
-    append(
-      openai_completions,
-      list(
-        completion$choices[[1]]$message
+  if(debug) message('Finished request...')
+  
+  if(resp$status_code < 400){
+    
+    completion <- httr::content(resp)
+    
+    openai_completions <<-
+      append(
+        openai_completions,
+        list(
+          completion$choices[[1]]$message
+        )
       )
-    )
-  
-  openai_completions_usage <<- completion$usage$total_tokens # Could be completion_tokens or prompt_tokens also.
-  
-  # completion <- 
-  #   openai::create_completion(prompt = prompt, 
-  #                             model = 'gpt-3.5-turbo', 
-  #                             best_of = 1, 
-  #                             top_p = 1,
-  #                             frequency_penalty = 0,
-  #                             presence_penalty = 0,
-  #                             temperature = 1,
-  #                             echo = T, 
-  #                             max_tokens = 4096 - nchar(prompt), 
-  #                             stream = F)$choices[[1]]$text
-
-  cat("\014")
-  rstudioapi::sendToConsole(code = paste0(prompt, completion$choices[[1]]$message$content) %>% 
-                              stringr::str_remove('^\n') %>% stringr::str_remove_all('\\`\\`\\`\\{r\\}') %>% str_remove_all('\\`\\`\\`') %>% str_trim(side = 'left'))
-  rstudioapi::sendToConsole("", execute = FALSE, echo = TRUE)
+    
+    openai_completions_usage <<- completion$usage$total_tokens # Could be completion_tokens or prompt_tokens also.
+    
+    # completion <- 
+    #   openai::create_completion(prompt = prompt, 
+    #                             model = 'gpt-3.5-turbo', 
+    #                             best_of = 1, 
+    #                             top_p = 1,
+    #                             frequency_penalty = 0,
+    #                             presence_penalty = 0,
+    #                             temperature = 1,
+    #                             echo = T, 
+    #                             max_tokens = 4096 - nchar(prompt), 
+    #                             stream = F)$choices[[1]]$text
+    
+    cat("\014")
+    rstudioapi::sendToConsole(code = paste0(prompt, completion$choices[[1]]$message$content) %>% 
+                                stringr::str_remove('^\n') %>% stringr::str_remove('^R') %>% stringr::str_remove_all('\\`\\`\\`\\{r\\}') %>% str_remove_all('\\`\\`\\`') %>% str_trim(side = 'left'))
+    rstudioapi::sendToConsole("", execute = FALSE, echo = TRUE)
+  }else{
+    
+    openai_completions <<-
+      openai_completions %>% 
+      head(-1)
+    
+    openai_completions_usage <<- 
+      openai_completions_usage - 
+      nchar(prompt)
+    
+    stop(httr::content(resp))
+  }
 }
